84e1ae6d8c7f5728006c8a49e19231f155b47565
JustinBrassard88 <91274492+JustinBrassard88@users.noreply.github.com>
2022-02-02 10:12:28 -0500

    Update CustomMap.js

--- story/CustomMap.js
+++ story/CustomMap.js
@@ -1,24 +1,14 @@
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("mymap"), {
-    zoom: 5,
-    center: { lat: 43.822, lng: -80.380 },
+  const map = new google.maps.Map(document.getElementById("mmymap"), {
+    zoom: 10,
+    center: { lat: -33.9, lng: 151.2 },
   });
 
   setMarkers(map);
 }
 
-/
+
 const beaches = [
-  ["Bondi Beach", -33.890542, 151.274856, 4],
-  ["Coogee Beach", -33.923036, 151.259052, 5],
-  ["Cronulla Beach", -34.028249, 151.157507, 3],
-  ["Manly Beach", -33.80010128657071, 151.28747820854187, 2],
-  ["Maroubra Beach", -33.950198, 151.259302, 1],
-  ["Bondi Beach", -33.890542, 151.274856, 4],
-  ["Coogee Beach", -33.923036, 151.259052, 5],
-  ["Cronulla Beach", -34.028249, 151.157507, 3],
-  ["Manly Beach", -33.80010128657071, 151.28747820854187, 2],
-  ["Maroubra Beach", -33.950198, 151.259302, 1],
   ["Bondi Beach", -33.890542, 151.274856, 4],
   ["Coogee Beach", -33.923036, 151.259052, 5],
   ["Cronulla Beach", -34.028249, 151.157507, 3],
@@ -26,7 +16,7 @@ const beaches = [
   ["Maroubra Beach", -33.950198, 151.259302, 1],
 ];
 
-function setMarkers(mymap) {
+function setMarkers(map) {
 
   const image = {
     url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
@@ -34,5 +24,22 @@ function setMarkers(mymap) {
     origin: new google.maps.Point(0, 0),
     anchor: new google.maps.Point(0, 32),
   };
- 
+
+  const shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: "poly",
+  };
+
+  for (let i = 0; i < beaches.length; i++) {
+    const beach = beaches[i];
+
+    new google.maps.Marker({
+      position: { lat: beach[1], lng: beach[2] },
+      map,
+      icon: image,
+      shape: shape,
+      title: beach[0],
+      zIndex: beach[3],
+    });
+  }
 }
